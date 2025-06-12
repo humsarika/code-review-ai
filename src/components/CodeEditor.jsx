7b01aa7846f71eaaf96fc084d0ee927fefe971cc
Sarika Kushwaha <sarika.vsa01@gmail.com>
2025-06-12 23:40:02 +0530

    Used useeffect and usestate for fontsize based on diffrent device sizes

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { useEffect,useState,React } from 'react';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/themes/prism-tomorrow.css';
 
 const CodeEditor = ({ code, setCode }) => {
+     const [fontSize, setFontSize] = useState(16);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        setFontSize(12); // mobile font
+      } else {
+        setFontSize(16); // normal font
+      }
+    };
+
+    handleResize(); // on mount
+    window.addEventListener('resize', handleResize); // on resize
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
     return (
         <div className='editor-container'>
             <Editor
@@ -14,7 +29,7 @@ const CodeEditor = ({ code, setCode }) => {
                 padding={15}
                 style={{
                     fontFamily: '"Fira Code", monospace',
-                    fontSize: 18,
+                    fontSize: fontSize,
                     backgroundColor: '#2d2d2d',
                     color: '#f8f8f2',
                     borderRadius: '8px',
